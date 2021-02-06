236d3c3096f2aa197f99996b4c14b3d7f590162c
mrmuke <m.xingster@gmail.com>
2021-02-06 15:39:49 -0600

    html

--- src/main/java/com/SpringApp/Website/Service/NotificationService.java
+++ src/main/java/com/SpringApp/Website/Service/NotificationService.java
@@ -1,5 +1,8 @@
 package com.SpringApp.Website.Service;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 import com.SpringApp.Website.AccessingData.ScheduleEvent;
 import com.SpringApp.Website.AccessingData.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,17 +10,18 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.mail.javamail.MimeMessageHelper;
 
 @Service
 public class NotificationService {
     private JavaMailSender javaMailSender;
+
     @Autowired
     public NotificationService(JavaMailSender javaMailSender) {
         this.javaMailSender = javaMailSender;
     }
 
-    public void sendNotification(User user, ScheduleEvent event) throws MailException {
-        SimpleMailMessage mail = new SimpleMailMessage();
+    public void sendNotification(User user, ScheduleEvent event) throws MailException, MessagingException {
         int place = 0;
         for(int i = 0;i<event.getUsersSignedUp().length;i++)
         {
@@ -27,10 +31,15 @@ public class NotificationService {
                 break;
             }
         }
-        mail.setTo(user.getEmailAddress());
-        mail.setFrom("lfaweekendactivities@gmail.com");
-        mail.setSubject("REMINDER:" + event.getName());
-        mail.setText("<strong><u>You have an activity soon:</u></strong><br/><b>Activity Name:</b> " + event.getName() + "<br/><b>Activity Time:</b> " + event.getTimeSlot() +"<br/>On this activity you are number " +((event.getPersonLimit()-place<0)?"WAITLIST":place)+ " on the list."+"<br/>If you no longer plan on coming, go to <a>wa.lfaapps.com</a> to cancel your sign up.");
-        javaMailSender.send(mail);
+        MimeMessage message = javaMailSender.createMimeMessage();
+
+        message.setSubject("REMINDER: " + event.getName());
+        MimeMessageHelper helper;
+        helper = new MimeMessageHelper(message, true);
+        helper.setFrom("lfaweekendactivities@gmail.com");
+        helper.setText("<strong><u>You have an activity soon:</u></strong><br/><b>Activity Name:</b> " + event.getName() + "<br/><b>Activity Time:</b> " + event.getTimeSlot() +"<br/>On this activity you are number " +((event.getPersonLimit()-place<0)?"WAITLIST":place)+ " on the list."+"<br/>If you no longer plan on coming, go to <a>wa.lfaapps.com</a> to cancel your sign up.", true);
+        helper.setTo(user.getEmailAddress());
+        
+        javaMailSender.send(message);
     }
 }
