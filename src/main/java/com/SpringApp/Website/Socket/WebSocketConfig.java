e188c85fed27e6c6bd4525c3b5a6e4170dcc6410
titut <conghoangbillnew@gmail.com>
2023-05-01 22:11:49 -0500

    Spring Websocket allow bigger messages

--- src/main/java/com/SpringApp/Website/Socket/WebSocketConfig.java
+++ src/main/java/com/SpringApp/Website/Socket/WebSocketConfig.java
@@ -1,10 +1,13 @@
 package com.SpringApp.Website.Socket;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry; 
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -13,15 +16,31 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
-        .addEndpoint("/connect")
+        .addEndpoint("/api/connect")
         .setAllowedOrigins("http://localhost:8081")
         .withSockJS();
     }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registry) {
+        registry.setMessageSizeLimit(200000); // default : 64 * 1024
+        registry.setSendTimeLimit(20 * 10000); // default : 10 * 10000
+        registry.setSendBufferSizeLimit(3* 512 * 1024); // default : 512 * 1024
+   }
  
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic/messages");
+        registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/ws");
     }
 
+    @Bean
+    public ServletServerContainerFactoryBean createServletServerContainerFactoryBean() {
+        ServletServerContainerFactoryBean factoryBean = new ServletServerContainerFactoryBean();
+        factoryBean.setMaxTextMessageBufferSize(2048 * 2048);
+        factoryBean.setMaxBinaryMessageBufferSize(2048 * 2048);
+        factoryBean.setMaxSessionIdleTimeout(2048L * 2048L);
+        factoryBean.setAsyncSendTimeout(2048L * 2048L);
+        return factoryBean;
+    }
 }
